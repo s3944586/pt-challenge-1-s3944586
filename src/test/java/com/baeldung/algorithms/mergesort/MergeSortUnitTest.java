9ca52549c39d0a97b1993b76d9572e04d02b3a67
Sebastian RODRIGUEZ <sebastian.rodriguez@rmit.edu.au>
2024-08-22 11:57:18 +1000

    feat2 change 2.

--- src/test/java/com/baeldung/algorithms/mergesort/MergeSortUnitTest.java
+++ src/test/java/com/baeldung/algorithms/mergesort/MergeSortUnitTest.java
@@ -8,7 +8,7 @@ public class MergeSortUnitTest {
 
     @Test
     public void positiveTest() {
-        int[] actual = { 5, 1, 6, 2, 3, 4 };
+        int[] actual = { 5, 1, 6, 2, 3, 4, 7 };
         int[] expected = { 1, 2, 3, 4, 5, 6 };
         MergeSort.mergeSort(actual, actual.length);
         Assert.assertArrayEquals(expected, actual);
