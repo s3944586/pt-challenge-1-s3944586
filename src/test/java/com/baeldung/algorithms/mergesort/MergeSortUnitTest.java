1922cab019e783c7c686f634f50fbad6f10e78a8
jake.w <jacobwoody32104@gmail.com>
2024-08-30 16:25:25 +1000

    Fixed bug in MergeSortUnitTest.java by retaining changes from hotfix/feat2

--- src/test/java/com/baeldung/algorithms/mergesort/MergeSortUnitTest.java
+++ src/test/java/com/baeldung/algorithms/mergesort/MergeSortUnitTest.java
@@ -8,7 +8,8 @@ public class MergeSortUnitTest {
 
     @Test
     public void positiveTest() {
-        int[] actual = { 5, 1, 6, 2, 3, 4, 7 }; // add an extra number
+        int[] actual = { 5, 1, 6, 2, 3, 4 }; // fix the bug
+
         int[] expected = { 1, 2, 3, 4, 5, 6 };
         MergeSort.mergeSort(actual, actual.length);
         Assert.assertArrayEquals(expected, actual);
